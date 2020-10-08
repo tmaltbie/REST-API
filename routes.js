c8df381e3ebc744ab05721ca877e7e8ac3ab499b
Trevor Maltbie <60497552+tmaltbie@users.noreply.github.com>
2020-10-07 21:29:36 -0700

    debugging user GET api/users 200 + credentials

--- routes.js
+++ routes.js
@@ -14,21 +14,28 @@ const authenticateUser = async(req, res, next) => {
 
     // Parse the user's credentials from the Authorization header.
     const credentials = auth(req)
+    console.log(credentials)
+
+    let debugUser = await User.findOne({ where: {
+        emailAddress: credentials.name
+    }})
+    console.log("debugUser:", debugUser.emailAddress)
+    console.log("debugUserPass:", debugUser.password)
 
     let user = await User.findOne({ where: {
-        emailAddress: credentials.username,
+        emailAddress: credentials.name,
         password: credentials.pass
     }})
 
     // If a user was successfully retrieved from the data store...
     if (credentials) {
-
         if (user) {
             const authenticated = bcryptjs
                 .compareSync(credentials.pass, user.password);
             if (authenticated) {
                 console.log(`Authentication successful for user: ${user.emailAddress}`);
                 req.currentUser = user;
+                
             } else {
                 message = `Authentication failure for user: ${user.emailAddress}`;
             }
@@ -41,32 +48,26 @@ const authenticateUser = async(req, res, next) => {
 
     // If user authenticaion failed...
     if (message) {
-
+        console.warn(message)
+        res.status(401).json({ message: 'Access Denied '});
+    } else {
+        next();
     }
-}
+};
 
 // Returns the currently authenticated user
 router.get('/users',  authenticateUser, (req, res) => {
-    // let user = await User.findOne({ where: { 
-    //     firstName: "Marie",
-    //     lastName: "Mancini",
-    //     emailAddress: 'marie@mancini.com'
-    // }})
     const user = req.currentUser;
+
     res.status(200).json({
-        name: `${user.firstName} ${user.lastName}`,
-        username: user.emailAddress,
+        login: user.emailAddress,
+        name: user.firstName
     });
   });
-
-
-
 
 router.get('/courses', async (req, res) => {
     const courses = await Course.findAll();
     res.json(courses);
   });
-
-
 
 module.exports = router;
